1d9de1ee72ceed225e1a790388a7e0ed3b1e8b0b
Adam Olsson <olsson.95_@hotmail.com>
2025-09-13 11:09:10 +0200

    works

--- cmake/llvm.cmake
+++ cmake/llvm.cmake
@@ -8,3 +8,16 @@ find_program(LLVM_CLANG_CXX
         /usr/local/bin
     NO_DEFAULT_PATH
 )
+
+if(LLVM_CLANG_CXX)
+    set(CMAKE_CXX_COMPILER ${LLVM_CLANG_CXX})
+     # Set libc++ for both compilation and linking globally
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -stdlib=libc++")
+    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -stdlib=libc++")
+
+    message(STATUS "Using LLVM Clang: ${LLVM_CLANG_CXX}")
+else()
+    message(FATAL_ERROR "LLVM Clang not found")
+endif()
+
