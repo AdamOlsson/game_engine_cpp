b8cf1ebe41d5a884ef4bbc0e689c7e35de5bb2c4
Adam Olsson <olsson.95_@hotmail.com>
2025-12-29 20:13:31 +0100

    wip: test to debug parsing of glyph outlines

---
+++ sdk/game_engine_sdk/render_engine/font/test/Type2Charstring_test.cpp
@@ -0,0 +1,15 @@
+#include "font/OTFFont.h"
+#include <gtest/gtest.h>
+
+#define ASSET_FILE(filename) ASSET_DIR "/" filename
+
+TEST(Type2CharstringTests, Test_ParseGlyphC) {
+    const font::OTFFont otf_font =
+        font::OTFFont(ASSET_FILE("rabbid-highway-sign-iv-bold-oblique.otf"));
+
+    const font::Glyph glyph = otf_font.glyphs[otf_font.glyph_index(font::Unicode("C"))];
+
+    // TODO: Create all triangles exactly as when I do in load_font()
+
+    EXPECT_FALSE(true);
+}
